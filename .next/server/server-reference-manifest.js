e50564c58241989795dbee97128a6e1345e005b1
Andrei D <codeandi@gmail.com>
2025-08-02 23:14:34 +0300

    restart fresh without scaffold

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KDwzCzGqCY7mLWsoeHnYVvc+GgrHscmrujj3KH8Eino=\"\n}"
--- packages/nextjs/components/scaffold-eth/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from "./Address/Address";
-export * from "./Balance";
-export * from "./BlockieAvatar";
-export * from "./Input";
-export * from "./RainbowKitCustomConnectButton";
